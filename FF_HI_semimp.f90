5f985f4c27913af036160b3ebf227e417ea5b651
isaacp <isaac.pincus@monash.edu>
2018-05-17 12:25:23 +1000

    Pre-OMP parallel code

--- FF_HI_semimp.f90
+++ FF_HI_semimp.f90
@@ -1,12 +1,15 @@
 program FF_HI_semimp
 implicit none
 real*8, parameter :: PI = 4*atan(1.0D0)
-integer, parameter :: Ndtwidths = 5
+integer, parameter :: Ndtwidths = 1
+integer, parameter :: Ntraj = 100000
 integer :: Nblocks, Nsteps, steps, block, time(1:8), kseed, i
-integer :: NTimeSteps, Ntraj, timestep, trajectories
-real*8 :: sr, b, h, a, Q0, Ql, Nrelax_times, dt, Beta, Bpsi, Bpsi2
-real*8, dimension(3,3) :: k, delT, tau
-real*8, dimension(3) :: Q, F
+integer :: NTimeSteps, timestep, trajectories
+real*8 :: sr, b, h, a, Q0, Nrelax_times, dt, Beta, Bpsi, Bpsi2
+real*8, dimension(3,3) :: k, delT
+real*8, dimension(Ntraj,3,3) :: tau
+real*8, dimension(Ntraj, 3) :: Q, F, dW
+real*8, dimension(Ntraj) :: Ql
 integer, dimension(:), allocatable :: seed
 real*8, dimension(Ndtwidths):: timestepwidths, Aeta, Veta, Apsi, Vpsi, Apsi2, Vpsi2
 
@@ -16,9 +19,9 @@ allocate(seed(1:kseed))
 forall (i=1:kseed) seed(i) =time(8)*100000+time(7)*1000+time(6)*10+time(5)+12341293*real(i)
 call random_seed(put=seed)
 
-Ntraj = 100000
 Nrelax_times = 1
-timestepwidths = (/0.5D0,0.4D0,0.3D0,0.2D0,0.1D0/)
+!timestepwidths = (/0.5D0,1.D0/3.D0,0.25D0,1.D0/12.D0,0.04D0/)
+timestepwidths = (/0.1D0/)
 sr = 1.D0
 b = 1000000000.D0
 h = 0.D0
@@ -29,7 +32,7 @@ k(:,:) = 0.D0
 k(1,2) = 1.D0
 k = sr*k
 
-tau(:,:) = 0.D0
+tau(:,:,:) = 0.D0
 
 delT(:,:) = 0.D0
 delT(1,1) = 1.D0
@@ -43,67 +46,75 @@ Veta = 0.D0
 Vpsi = 0.D0
 Vpsi2 = 0.D0
 
-Q(:) = (/1,1,1/)
-
 looptimesteps: do timestep=1,Ndtwidths
 
     dt = timestepwidths(timestep)
     NtimeSteps = int(Nrelax_times/dt)
 
-    looptraj: do trajectories=1,Ntraj
+    do i=1,Ntraj
+        tau(i,:,:) = 0.D0
+        Q(i,:) = Wiener_step(dt)/sqrt(dt)
+    end do
 
-        tau(:,:) = 0.D0
 
-        equil: do steps=1,int(10/dt)
-            k(:,:) = 0.D0
-            call step(Q, k, dt, Q0, delT, b, a)
-        end do equil
 
-        do steps=1,Ntimesteps
-            k(1,2) = sr
-            call step(Q, k, dt, Q0, delT, b, a)
+    do steps=1,Ntimesteps
+        k(1,2) = sr
+
+        do i=1,Ntraj
+            dW(i,:) = Wiener_step(dt)
         end do
 
-        Ql = sqrt(Q(1)**2 + Q(2)**2 + Q(3)**2)
-        F(:) = (Ql - Q0)/(1.0D0-(Ql-Q0)**2/b)*Q(:)/Ql
-        tau = tau + dyadic_prod(Q,F)
+        forall(i=1:Ntraj)
+            Q(i,:) =  step(Q(i,:), k, dt, Q0, delT, b, a, dW(i,:))
+        end forall
+    end do
 
-        Beta = tau(1,2)/sr
-        Bpsi = (tau(1,1) - tau(2,2))/sr**2
-        Bpsi2 = (tau(2,2) - tau(3,3))/sr**2
-        Aeta(timestep) = Aeta(timestep) + Beta
-        Apsi(timestep) = Apsi(timestep) + Bpsi
-        Apsi2(timestep) = Apsi2(timestep) + Bpsi2
-        Veta(timestep) = Veta(timestep) + Beta**2
-        Vpsi(timestep) = Vpsi(timestep) + Bpsi**2
-        Vpsi2(timestep) = Vpsi2(timestep) + Bpsi2**2
+    forall(i=1:Ntraj)
+        Ql(i) = sqrt(Q(i,1)**2 + Q(i,2)**2 + Q(i,3)**2)
+        F(i,:) = (Ql(i) - Q0)/(1.0D0-(Ql(i)-Q0)**2/b)*Q(i,:)/Ql(i)
+        tau(i,:,:) = tau(1,:,:) + dyadic_prod(Q(i,:),F(i,:))
+    end forall
 
-    end do looptraj
+!    Beta = tau(1,2)/sr
+!    Bpsi = (tau(1,1) - tau(2,2))/sr**2
+!    Bpsi2 = (tau(2,2) - tau(3,3))/sr**2
+!    Aeta(timestep) = Aeta(timestep) + Beta
+!    Apsi(timestep) = Apsi(timestep) + Bpsi
+!    Apsi2(timestep) = Apsi2(timestep) + Bpsi2
+!    Veta(timestep) = Veta(timestep) + Beta**2
+!    Vpsi(timestep) = Vpsi(timestep) + Bpsi**2
+!    Vpsi2(timestep) = Vpsi2(timestep) + Bpsi2**2
 
 end do looptimesteps
 
-Aeta = Aeta/Ntraj
-Veta = Veta/Ntraj
-Veta = sqrt((Veta - Aeta**2)/(Ntraj-1))
+!write(*,'(F7.4,2X,F7.4,2X,F7.4)') Q
+!write(*,*) 'dW'
+!write(*,'(F7.4,2X,F7.4,2X,F7.4)') dW
+!write(*,*) Q
 
-Apsi = Apsi/Ntraj
-Vpsi = Vpsi/Ntraj
-Vpsi = sqrt((Vpsi - Apsi**2)/(Ntraj-1))
-
-Apsi2 = Apsi2/Ntraj
-Vpsi2 = Vpsi2/Ntraj
-Vpsi2 = sqrt((Vpsi2 - Apsi2**2)/(Ntraj-1))
-
-
-10 format(F4.2,4X,F7.5,4X,F7.5,4X,F7.5,4X,F7.5,4X,F7.5,4X,F7.5,4X)
-do i = 1,Ndtwidths
-    write(*,10) timestepwidths(i), Aeta(i), Veta(i), Apsi(i), Vpsi(i), Apsi2(i), Vpsi2(i)
-end do
+!Aeta = Aeta/Ntraj
+!Veta = Veta/Ntraj
+!Veta = sqrt((Veta - Aeta**2)/(Ntraj-1))
+!
+!Apsi = Apsi/Ntraj
+!Vpsi = Vpsi/Ntraj
+!Vpsi = sqrt((Vpsi - Apsi**2)/(Ntraj-1))
+!
+!Apsi2 = Apsi2/Ntraj
+!Vpsi2 = Vpsi2/Ntraj
+!Vpsi2 = sqrt((Vpsi2 - Apsi2**2)/(Ntraj-1))
+!
+!
+!10 format(F4.2,4X,F7.5,4X,F7.5,4X,F7.5,4X,F7.5,4X,F7.5,4X,F7.5,4X)
+!do i = 1,Ndtwidths
+!    write(*,10) timestepwidths(i), Aeta(i), Veta(i), Apsi(i), Vpsi(i), Apsi2(i), Vpsi2(i)
+!end do
 
 
 contains
 
-function construct_B(Q, a, delT)
+pure function construct_B(Q, a, delT)
     implicit none
     real*8, intent(in) :: Q(3), a
     real*8, intent(in) :: delT(3,3)
@@ -140,20 +151,19 @@ function Wiener_step(dt)
 
 end function Wiener_step
 
-subroutine step(Q, k, dt, Q0, delT, b, a)
+pure function step(Q, k, dt, Q0, delT, b, a, dW)
     implicit none
-    real*8, intent(in) :: dt, Q0, b, a
+    real*8, intent(in) :: Q(3), dt, Q0, b, a, dW(3)
     real*8, intent(in) :: delT(3,3), k(3,3)
     real*8 :: Ql, L, RdotB2_L, Qlength, temp_R1, temp_R2
-    real*8, dimension(3) :: F, dW, u, RHS, Qpred, RdotB2
+    real*8, dimension(3) :: F, u, RHS, Qpred, RdotB2
     real*8, dimension(3,3) :: B1, B2, B1B1, B2B2
-    real*8 :: Q(3)
+    real*8, dimension(3) :: step
 
     Ql = sqrt(Q(1)**2 + Q(2)**2 + Q(3)**2)
     F = (Ql - Q0)/(1.0D0-(Ql-Q0)**2/b)*Q/Ql
 
     B1 = construct_B(Q, a, delT)
-    dW = Wiener_step(dt)
 
     B1B1 = matmul(B1,B1)
     Qpred = Q(:) + (ten_vec_dot(k,Q) - 0.5D0*ten_vec_dot(B1B1,F))*dt &
@@ -172,11 +182,11 @@ subroutine step(Q, k, dt, Q0, delT, b, a)
     Qlength = find_roots( -(2.D0*Q0+L), -b+Q0**2-RdotB2_L+2.d0*L*Q0, &
                             RdotB2_L*Q0+L*(b-Q0**2), Q0, sqrt(b) )
 
-    Q = RHS*Qlength/L
+    step = RHS*Qlength/L
 
-end subroutine step
+end function step
 
-function find_roots(a, b, c, Q0, lim)
+pure function find_roots(a, b, c, Q0, lim)
     implicit none
     real*8, parameter :: PI = 4*atan(1.0D0)
     real*8, intent(in) :: a, b, c, Q0, lim
@@ -198,7 +208,7 @@ function find_roots(a, b, c, Q0, lim)
 
 end function find_roots
 
-function dyadic_prod(vec1,vec2)
+pure function dyadic_prod(vec1,vec2)
     implicit none
     real*8, dimension(3), intent(in) :: vec1, vec2
     real*8, dimension(3,3) :: dyadic_prod
@@ -211,7 +221,7 @@ function dyadic_prod(vec1,vec2)
     end do
 end function dyadic_prod
 
-function ten_vec_dot(tensor, vector)
+pure function ten_vec_dot(tensor, vector)
     implicit none
     real*8, dimension(3,3), intent(in) :: tensor
     real*8, dimension(3), intent(in) :: vector
