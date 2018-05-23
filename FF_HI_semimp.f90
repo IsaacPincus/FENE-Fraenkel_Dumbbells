a2e127b81de4510077defd7e6ace9ff90a025ef4
isaacp <isaac.pincus@monash.edu>
2018-05-23 12:34:41 +1000

    Equilibrium testing code

--- FF_HI_semimp.f90
+++ FF_HI_semimp.f90
@@ -3,16 +3,17 @@ use omp_lib
 implicit none
 real*8, parameter :: PI = 4*atan(1.0D0)
 integer, parameter :: Ndtwidths = 5
-integer, parameter :: Ntraj = 1000000
+integer, parameter :: Ntraj = 80000
 integer*8 :: Nblocks, Nsteps, steps, block, time(1:8), seed, i
 integer :: NTimeSteps, timestep, trajectories
-real*8 :: sr, b, h, a, Q0, Nrelax_times, dt, Ql, F(3), dW(3)
+real*8 :: sr, b, h, a, Q0, Nrelax_times, dt, Ql, F(3), dW(3), Qtemp(3)
+real*8 :: a2, a4, C43, C83, C143
 real*8, dimension(3,3) :: k, delT
 !large arrays must be declared allocatable so they go into heap, otherwise
 !OpenMP threads run out of memory
 real*8, dimension(:,:,:), allocatable :: tau
 real*8, dimension(:, :), allocatable :: Q
-real*8, dimension(Ndtwidths):: timestepwidths, Aeta, Veta, Apsi, Vpsi, Apsi2, Vpsi2
+real*8, dimension(Ndtwidths):: timestepwidths, Aeta, Veta, Apsi, Vpsi, Apsi2, Vpsi2, Qavg, Vqavg
 
 call date_and_time(values=time)
 seed = time(8)*100 + time(7)*10
@@ -20,13 +21,20 @@ seed = time(8)*100 + time(7)*10
 allocate(tau(3,3,1:Ntraj))
 allocate(Q(3,1:Ntraj))
 
-Nrelax_times = 10
+Nrelax_times = 100
 timestepwidths = (/0.5D0,1.D0/3.D0,0.25D0,1.D0/12.D0,0.04D0/)
-sr = 1.D0
-b = 1000000000.D0
+!timestepwidths = (/0.01D0/)
+sr = 0.D0
+b = 1.D0
 h = 0.D0
 a = h*sqrt(PI)
 Q0 = 0.D0
+
+a2 = a**2
+a4 = a**4
+C43 = 4.D0/3.D0
+C83 = 8.D0/3.D0
+C143 = 14.D0/3.D0
 
 k(:,:) = 0.D0
 k(1,2) = 1.D0
@@ -46,25 +54,23 @@ Veta = 0.D0
 Vpsi = 0.D0
 Vpsi2 = 0.D0
 
-!$OMP PARALLEL PRIVATE(seed, Ql, F)
-seed = seed + 932117 + OMP_get_thread_num()*2685821657736338717_8
-
 looptimesteps: do timestep=1,Ndtwidths
 
     dt = timestepwidths(timestep)
     NtimeSteps = int(Nrelax_times/dt)
 
-    !$OMP DO
     do i=1,Ntraj
         tau(:,:,i) = 0.D0
-        Q(:,i) = Wiener_step(seed, dt)/sqrt(dt)
+        Q(:,i) = (/-1.D0, -1.D0, -1.D0/)
     end do
-    !$OMP END DO
+
+    !$OMP PARALLEL DEFAULT(firstprivate) SHARED(Q)
+    !$ seed = seed + 932117 + OMP_get_thread_num()*2685821657736338717_8
 
     do steps=1,Ntimesteps
         k(1,2) = sr
 
-        !$OMP DO
+        !$OMP DO schedule(dynamic, 100)
         do i=1,Ntraj
             Q(:,i) =  step(Q(:,i), k, dt, Q0, delT, b, a)
         end do
@@ -72,45 +78,54 @@ looptimesteps: do timestep=1,Ndtwidths
 
     end do
 
-    !$OMP DO
-    do i=1,Ntraj
-        Ql = sqrt(Q(1,i)**2 + Q(2,i)**2 + Q(3,i)**2)
-        F = (Ql - Q0)/(1.0D0-(Ql-Q0)**2/b)*Q(:,i)/Ql
-        tau(:,:,i) = tau(:,:,i) + dyadic_prod(Q(:,i),F)
-    end do
-    !$OMP END DO
+    !$OMP END PARALLEL
 
-    !$OMP WORKSHARE
-    Aeta(timestep) = sum(tau(1,2,:))/sr
-    Apsi(timestep) = sum((tau(1,1,:) - tau(2,2,:)))/sr**2
-    Apsi2(timestep) = sum((tau(2,2,:) - tau(3,3,:)))/sr**2
-    Veta(timestep) = sum(tau(1,2,:)**2)/sr**2
-    Vpsi(timestep) = sum(((tau(1,1,:) - tau(2,2,:)))**2)/sr**4
-    Vpsi2(timestep) = sum(((tau(2,2,:) - tau(3,3,:)))**2)/sr**4
-    !$OMP END WORKSHARE
+!    do i=1,Ntraj
+!        Ql = sqrt(Q(1,i)**2 + Q(2,i)**2 + Q(3,i)**2)
+!        F = (Ql - Q0)/(1.0D0-(Ql-Q0)**2/b)*Q(:,i)/Ql
+!        tau(:,:,i) = tau(:,:,i) + dyadic_prod(Q(:,i),F)
+!    end do
+
+!    Aeta(timestep) = sum(tau(1,2,:))/sr
+!    Apsi(timestep) = sum((tau(1,1,:) - tau(2,2,:)))/sr**2
+!    Apsi2(timestep) = sum((tau(2,2,:) - tau(3,3,:)))/sr**2
+!    Veta(timestep) = sum(tau(1,2,:)**2)/sr**2
+!    Vpsi(timestep) = sum(((tau(1,1,:) - tau(2,2,:)))**2)/sr**4
+!    Vpsi2(timestep) = sum(((tau(2,2,:) - tau(3,3,:)))**2)/sr**4
+    Qavg(timestep) = sum(Q(1,:)**2 + Q(2,:)**2 + Q(3,:)**2)
+    Vqavg(timestep) = sum(sqrt(Q(1,:)**2 + Q(2,:)**2 + Q(3,:)**2))
 
 end do looptimesteps
-!$OMP END PARALLEL
 
 deallocate(tau)
 deallocate(Q)
 
-Aeta = Aeta/Ntraj
-Veta = Veta/Ntraj
-Veta = sqrt((Veta - Aeta**2)/(Ntraj-1))
+!Aeta = Aeta/Ntraj
+!Veta = Veta/Ntraj
+!Veta = sqrt((Veta - Aeta**2)/(Ntraj-1))
+!
+!Apsi = Apsi/Ntraj
+!Vpsi = Vpsi/Ntraj
+!Vpsi = sqrt((Vpsi - Apsi**2)/(Ntraj-1))
+!
+!Apsi2 = Apsi2/Ntraj
+!Vpsi2 = Vpsi2/Ntraj
+!Vpsi2 = sqrt((Vpsi2 - Apsi2**2)/(Ntraj-1))
 
-Apsi = Apsi/Ntraj
-Vpsi = Vpsi/Ntraj
-Vpsi = sqrt((Vpsi - Apsi**2)/(Ntraj-1))
-
-Apsi2 = Apsi2/Ntraj
-Vpsi2 = Vpsi2/Ntraj
-Vpsi2 = sqrt((Vpsi2 - Apsi2**2)/(Ntraj-1))
+Qavg = sqrt(Qavg/Ntraj)
+Vqavg = Vqavg/Ntraj
+Vqavg = sqrt((Qavg**2 - Vqavg**2)/(Ntraj-1))
 
 
-10 format(F4.2,4X,F7.5,4X,F7.5,4X,F7.5,4X,F7.5,4X,F7.5,4X,F7.5,4X)
+!10 format(F4.2,4X,F7.5,4X,F7.5,4X,F7.5,4X,F7.5,4X,F7.5,4X,F7.5,4X,F7.5,4X,F7.5)
+!do i = 1,Ndtwidths
+!    write(*,10) timestepwidths(i), Aeta(i), Veta(i), Apsi(i), Vpsi(i), &
+!                Apsi2(i), Vpsi2(i), Qavg(i), Vqavg(i)
+!end do
+
+10 format(F4.2,4X,F7.5,4X,F7.5)
 do i = 1,Ndtwidths
-    write(*,10) timestepwidths(i), Aeta(i), Veta(i), Apsi(i), Vpsi(i), Apsi2(i), Vpsi2(i)
+    write(*,10) timestepwidths(i), Qavg(i), Vqavg(i)
 end do
 
 contains
@@ -120,19 +135,16 @@ pure function construct_B(Q, a, delT)
     real*8, intent(in) :: Q(3), a
     real*8, intent(in) :: delT(3,3)
     real*8, dimension(3,3) :: construct_B
-    real*8 :: Ql, aux, g, g_til, a2, a4, Ql2, Ql4, Ql6, C43
+    real*8 :: Ql, aux, g, g_til, Ql2, Ql4, Ql6
 
-    Ql = sqrt(Q(1)**2 + Q(2)**2 + Q(3)**2)
-    Ql2 = Ql**2
-    Ql4 = Ql**4
-    Ql6 = Ql**6
-    a2 = a**2
-    a4 = a**4
-    C43 = 4.D0/3.D0
+    Ql2 = Q(1)**2 + Q(2)**2 + Q(3)**2
+    Ql = sqrt(Ql2)
+    Ql4 = Ql2**2
+    Ql6 = Ql2**3
 
     aux = a/(C43*Ql*(Ql2 + C43*a2)**3)
-    g = 1.D0-aux*(Ql6 + 14.D0/3.D0*a2*Ql4 + 8.D0*a4*Ql2)
-    g_til = -aux*(Ql6 + 2.D0*a2*Ql4 - 8.D0/3.D0*a4*Ql2)
+    g = 1.D0-aux*(Ql6 + C143*a2*Ql4 + 8.D0*a4*Ql2)
+    g_til = -aux*(Ql6 + 2.D0*a2*Ql4 - C83*a4*Ql2)
     construct_B = sqrt(g)*delT + (sqrt(g+g_til)-sqrt(g))*dyadic_prod(Q,Q)/Ql2
     return
 
@@ -160,7 +172,7 @@ function Wiener_step(seed, dt)
     end do
 
     !Generates an approximately gaussian-distributed number dW
-    Wiener_step = dW*(14.14855378D0*sqrt(dt)*dW*dW + 1.21569221D0*sqrt(dt))
+    Wiener_step = dW*sqrt(dt)*(14.14855378D0*dW*dW + 1.21569221D0)
     return
 
 end function Wiener_step
@@ -197,6 +209,14 @@ function step(Q, k, dt, Q0, delT, b, a)
     Qlength = find_roots( -(2.D0*Q0+L), -b+Q0**2-RdotB2_L+2.d0*L*Q0, &
                             RdotB2_L*Q0+L*(b-Q0**2), Q0, sqrt(b) )
 
+    !Check that Qlength is +ve, otherwise Q won't point in direction of RHS
+    if (Qlength.le.0) then
+        write(*,*) 'Qlength =< 0'
+        write(*,*) Qlength, Q
+        Qlength = find_roots( -(2.D0*Q0-L), -b+Q0**2-RdotB2_L-2.d0*L*Q0, &
+                            RdotB2_L*Q0-L*(b-Q0**2), Q0, sqrt(b) )
+    endif
+
     step = RHS*Qlength/L
 
 end function step
@@ -216,9 +236,9 @@ pure function find_roots(a, b, c, Q0, lim)
 
     do i=-1,1
         x = -2.D0*sqrt(Q)*cos((theta + real(i)*PI*2.D0)/3.D0)-a/3.D0
-        if ((x.ge.(Q0-lim)).and.(x.le.(Q0+lim))) then
+        if ((x.ge.(Q0)).and.(x.le.(Q0+lim))) then
             find_roots = x
-        endif
+        end if
    end do
 
 end function find_roots
@@ -229,8 +249,8 @@ pure function dyadic_prod(vec1,vec2)
     real*8, dimension(3,3) :: dyadic_prod
     integer :: i, j
 
-    do j=1,3
-        do i=1,3
+    do i=1,3
+        do j=1,3
             dyadic_prod(i,j) = vec1(i)*vec2(j)
         end do
     end do
